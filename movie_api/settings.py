3cf975211b8c5d7811394fd8b5b07adcffa3cc46
Kateryna Bakhchedzhy <k.bakhchedzhy@gmail.com>
2021-03-30 01:37:18 +0300

    connected to postgresql

--- movie_api/settings.py
+++ movie_api/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'movie_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'movie_api',
+        'USER': 'kbakhchedzhy',
+        'PASSWORD': 'katerynab',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
