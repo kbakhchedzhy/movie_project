69dc4e8cbac0c9c163ad187a2dfbbc405a2b3c2b
Kateryna Bakhchedzhy <k.bakhchedzhy@gmail.com>
2021-04-21 23:57:19 +0300

    command save new movies in db moviedetails and its relations

--- home/management/commands/MovieForTime.py
+++ home/management/commands/MovieForTime.py
@@ -4,6 +4,8 @@ from datetime import date
 from django.core.management.base import BaseCommand
 from tmdbv3api import Discover, Movie
 
+from home.models import MovieDetails, Genres, ProductionCompany, SpokenLanguage
+
 
 class Command(BaseCommand):
 
@@ -21,8 +23,50 @@ class Command(BaseCommand):
             movie = Movie()
             all_movies = dis.discover_movies(params={'release_date.gte': options['begin'], 'release_date.lte': options['end']})
             for this_movie in all_movies:
-                print(movie.details(movie_id=this_movie.id))
-            print("\n\n\n")
+                movie_db = MovieDetails()
+                movie_details = movie.details(movie_id=this_movie.id)
+                movie_db.backdrop_path = movie_details.backdrop_path
+                movie_db.budget = movie_details.budget
+                movie_db.id = movie_details.id
+                movie_db.imdb_id = movie_details.imdb_id
+                movie_db.original_language = movie_details.original_language
+                movie_db.original_title = movie_details.original_title
+                movie_db.overview = movie_details.overview
+                movie_db.popularity = movie_details.popularity
+                movie_db.poster_path = movie_details.poster_path
+                movie_db.release_date = movie_details.release_date
+                movie_db.revenue = movie_details.revenue
+                movie_db.runtime = movie_details.runtime# movie_db.spoken_languages = movie_details.spoken_languages
+                movie_db.status = movie_details.status
+                movie_db.tagline = movie_details.tagline
+                movie_db.title = movie_details.title
+                movie_db.video = movie_details.video
+                movie_db.vote_average = movie_details.vote_average
+                movie_db.vote_count = movie_details.vote_count
+
+                movie_db.save()
+
+                for one_of_genres in movie_details.genres:
+                    genre, _ = Genres.objects.get_or_create(
+                        id=one_of_genres.id,
+                        title=one_of_genres.name)
+                    movie_db.genres.add(genre)
+
+                for one_of_prodcompany in movie_details.production_companies:
+                    prodcompany, _ = ProductionCompany.objects.get_or_create(
+                        id=one_of_prodcompany.id,
+                        name=one_of_prodcompany.name,
+                        logo_path=one_of_prodcompany.logo_path,
+                        origin_country=one_of_prodcompany.origin_country)
+                    movie_db.production_companies.add(prodcompany)
+
+                for one_of_sp_language in movie_details.spoken_languages:
+                    sp_language, _ = SpokenLanguage.objects.get_or_create(
+                        iso_639_1=one_of_sp_language.iso_639_1,
+                        name=one_of_sp_language.name)
+                    movie_db.spoken_languages.add(sp_language)
+
+            self.stdout.write('Finished\n')
 
         else:
             self.stdout.write('Please, enter period of time.')
